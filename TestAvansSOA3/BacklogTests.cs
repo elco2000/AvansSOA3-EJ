ba086660675a0dc29acef5bea28593973c9bcf88
Elco Mussert <elcomussert@gmail.com>
2022-03-24 15:52:13 +0100

    Added a few tests with members

--- TestAvansSOA3/BacklogTests.cs
+++ TestAvansSOA3/BacklogTests.cs
@@ -27,8 +27,96 @@ namespace TestAvansSOA3
         }
 
         [TestMethod]
-        // TC-BL11: Binnen een backlog item moet de mogelijkheid zijn om meer activiteiten aan te maken.
-        public void TestTCBL11()
+        // TC-BL2: Maximaal één developer kan gekoppeld zijn aan een backlog item.
+        public void TestTCBL2()
+        {
+            // Arrange
+            BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
+                
+            MemberFactory factory = new MemberFactory();
+            IMember developer = factory.GetMember("developer");
+
+            // Act
+            developer.SetRole("developer");
+
+            backlogItem.AddMember(developer);
+
+            string expectedResult = "developer";
+
+            // Assert
+            Assert.AreEqual(expectedResult, backlogItem.GetMember().GetRole());
+        }
+
+        [TestMethod]
+        // TC-BL3: Maximaal één developer kan gekoppeld zijn aan een backlog item.
+        public void TestTCBL3()
+        {
+            // Arrange
+            BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
+
+            MemberFactory factory = new MemberFactory();
+            IMember developerOne = factory.GetMember("developer");
+            IMember developerTwo = factory.GetMember("developer");
+
+            // Act
+            developerOne.SetRole("developer");
+            developerOne.SetName("Piet");
+
+            developerTwo.SetRole("developer");
+            developerTwo.SetName("Freek");
+
+            backlogItem.AddMember(developerOne);
+            backlogItem.AddMember(developerTwo);
+
+            string expectedResult = "Piet";
+
+            // Assert
+            Assert.AreEqual(expectedResult, backlogItem.GetMember().GetName());
+        }
+
+        [TestMethod]
+        // TC-BL4: Maximaal één developer kan gekoppeld zijn aan een backlog item.
+        public void TestTCBL4()
+        {
+            // Arrange
+            BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
+
+            MemberFactory factory = new MemberFactory();
+            IMember scrumMaster = factory.GetMember("scrum master");
+
+            // Act
+            scrumMaster.SetRole("scrum master");
+
+            backlogItem.AddMember(scrumMaster);
+
+            // Assert
+            Assert.AreEqual(null, backlogItem.GetMember());
+        }
+
+        [TestMethod]
+        // TC-BL5: Developer kan verwijderd worden uit een backlog item.
+        public void TestTCBL5()
+        {
+            // Arrange
+            BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
+
+            MemberFactory factory = new MemberFactory();
+            IMember developer = factory.GetMember("developer");
+
+            // Act
+            developer.SetRole("developer");
+
+            backlogItem.AddMember(developer);
+            backlogItem.RemoveMember();
+
+
+            // Assert
+            Assert.AreEqual(null, backlogItem.GetMember());
+        }
+
+        [TestMethod]
+        // TC-BL12: Binnen een backlog item moet de mogelijkheid zijn om meer activiteiten aan te maken.
+        public void TestTCBL12()
         {
             // Arrange
             BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
@@ -43,8 +131,8 @@ namespace TestAvansSOA3
         }
 
         [TestMethod]
-        // TC-BL14: De backlog item kan als done gezien worden, indien alle onderliggende activiteiten voltooid zijn. 
-        public void TestTCBL14()
+        // TC-BL15: De backlog item kan als done gezien worden, indien alle onderliggende activiteiten voltooid zijn. 
+        public void TestTCBL15()
         {
             // Arrange
             BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
@@ -70,8 +158,8 @@ namespace TestAvansSOA3
         }
 
         [TestMethod]
-        // TC-BL15: De backlog item kan als done gezien worden, indien alle onderliggende activiteiten voltooid zijn. 
-        public void TestTCBL15()
+        // TC-BL16: De backlog item kan als done gezien worden, indien alle onderliggende activiteiten voltooid zijn. 
+        public void TestTCBL16()
         {
             // Arrange
             BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
@@ -89,8 +177,8 @@ namespace TestAvansSOA3
         }
 
         [TestMethod]
-        // TC-BL22: Een lid van het project kan een discussie starten bij een bepaalde backlog item.
-        public void TestTCBL22()
+        // TC-BL23: Een lid van het project kan een discussie starten bij een bepaalde backlog item.
+        public void TestTCBL23()
         {
             // Arrange
             BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
@@ -105,8 +193,8 @@ namespace TestAvansSOA3
         }
 
         [TestMethod]
-        // TC-BL23: Een lid van het project kan reacties plaatsen bij een discussie bij een bepaalde backlog item.
-        public void TestTCBL23()
+        // TC-BL24: Een lid van het project kan reacties plaatsen bij een discussie bij een bepaalde backlog item.
+        public void TestTCBL24()
         {
             // Arrange
             BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
@@ -125,8 +213,8 @@ namespace TestAvansSOA3
         }
 
         [TestMethod]
-        // TC-BL24: Een lid van het project kan geen reacties meer plaatsen bij een discussie van een backlogitem als deze discussie gesloten is.
-        public void TestTCBL24()
+        // TC-BL25: Een lid van het project kan geen reacties meer plaatsen bij een discussie van een backlogitem als deze discussie gesloten is.
+        public void TestTCBL25()
         {
             // Arrange
             BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
@@ -147,8 +235,8 @@ namespace TestAvansSOA3
         }
 
         [TestMethod]
-        // TC-BL25: Een lid van het project kan geen discussie meer aanpassen bij een backlog item als deze discussie gesloten is.
-        public void TestTCBL25()
+        // TC-BL26: Een lid van het project kan geen discussie meer aanpassen bij een backlog item als deze discussie gesloten is.
+        public void TestTCBL26()
         {
             // Arrange
             BacklogItem backlogItem = new BacklogItem("1. BacklogItem", "Doing some code");
