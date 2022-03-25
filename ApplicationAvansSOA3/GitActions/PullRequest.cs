e34034ff651b5998631eff2c5dacf9a418dcdd0d
Elco Mussert <elcomussert@gmail.com>
2022-03-25 16:56:12 +0100

    Added Git Action code and tests.

---
+++ ApplicationAvansSOA3/GitActions/PullRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationAvansSOA3.GitActions
+{
+    public class PullRequest : IGitBehavior
+    {
+        public void Action()
+        {
+            Console.Write("Git Action: PullRequest wordt uitgevoerd. Code wordt nu gepusht naar de gewenste branch.");
+        }
+    }
+}
