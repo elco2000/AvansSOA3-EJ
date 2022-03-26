39be0c762a267b08e5256e4c09fc230ae75cc467
Elco Mussert <elcomussert@gmail.com>
2022-03-26 16:25:49 +0100

    Coverage fix

--- ApplicationAvansSOA3/CompositePipeline/Visitor.cs
+++ ApplicationAvansSOA3/CompositePipeline/Visitor.cs
@@ -15,5 +15,7 @@ namespace ApplicationAvansSOA3.CompositePipeline
         public abstract void VisitPackage(Package package);
         public abstract void VisitTest(Test test);
         public abstract void VisitUtility(Utility utility);
+
+        public abstract void SendEmptyLine();
     }
 }
